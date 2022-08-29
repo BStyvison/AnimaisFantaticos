81c90367ad4c8f87215ea1a7b6839266572bd18b
Brayan Styvison <99987766+BStyvison@users.noreply.github.com>
2022-08-29 11:53:44 -0300

    final

--- script.js
+++
@@ -1,5 +0,0 @@
-const animais = document.getElementById('animais')
-console.log(animais)
-
-const gridSection = document.getElementsByClassName('grid-section')
-console.log(gridSection[0].innerText)
